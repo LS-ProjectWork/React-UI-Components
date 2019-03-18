e149225862156e76a1cc439c529ab16b80e5204b
dpayton23 <darien.payton71@gmail.com>
2019-03-18 17:22:59 -0400

    updated imports

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Card.css';
-import './CardBanner';
-import './CardContent';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
 
 const CardContainer = () => {
     return (
