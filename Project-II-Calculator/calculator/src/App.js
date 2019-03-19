662e9ab3e3acd7610bfb68bd0000af94005f238c
dpayton23 <darien.payton71@gmail.com>
2019-03-19 16:14:30 -0400

    created number array

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 const App = () => {
   return (
     <div>
+      <CalculatorDisplay />
       <NumberButton />
       <ActionButton />
       <ActionButton />
