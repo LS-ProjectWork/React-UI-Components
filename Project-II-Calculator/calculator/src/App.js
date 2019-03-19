5d85765850727c4cefdfda6fc6cd9e728113739f
dpayton23 <darien.payton71@gmail.com>
2019-03-19 16:32:49 -0400

    added state variable

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -7,13 +7,14 @@ import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay'
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 class App extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {totalDisplay: 0}
+  }
   render() {
     return (
       <div>
-        <CalculatorDisplay />
-        <NumberButton />
-        <ActionButton />
-        <ActionButton />
+        <CalculatorDisplay display={this.state.totalDisplay} />
         <NumberButton buttonStyle="btn-wide" text={'clear'} />
         {numbers.map(num => <NumberButton text={num}/>)}
         <NumberButton buttonStyle="btn-wide" text={0} />
